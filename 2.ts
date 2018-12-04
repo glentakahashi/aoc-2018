da0fc17b1b790df5b4846fef2cb150742e88436c
Glen Takahashi <glen@glentaka.com>
2018-12-04 15:05:25 -0500

    Days

---
+++ 2.ts
@@ -0,0 +1,295 @@
+let a = `myhposlqgeauywfikztndcvrqr
+mbhposlxfeauywoikztndcvjqi
+mbhpoulxgeagywfikytndcvjqr
+jbhposlxgeauywdikztndcvjqk
+mbhpsslxueauywfikzfndcvjqr
+mbhposnxgeauzyfikztndcvjqr
+ibhposlxgetvywfikztndcvjqr
+mbcposlxgeauywfikztxdcvjqv
+mlhposltgeauywfikitndcvjqr
+mbhpostxgeauywfikztndjvjqy
+mboboslxglauywfikztndcvjqr
+mbhpoglxgeahywfikztndcvjqp
+mbhposlxgeapydpikztndcvjqr
+mbhposlxseauywfikztnncljqr
+mbhposlxgeauydfisztndcvjqj
+mbhposlxgeaugwwikzlndcvjqr
+mbhpoklxgeauywfikztndvvmqr
+mbhposlxgeauywfikdtndcmjqx
+mbhposlxaeauapfikztndcvjqr
+mbwposgxgeauymfikztndcvjqr
+mbhposlxgeauvwfirzcndcvjqr
+mbhpozlxgeaqywfykztndcvjqr
+mahqoslxgeauywfikzgndcvjqr
+mbhposlcgexbywfikztndcvjqr
+ykhposlxgeeuywfikztndcvjqr
+mbhgoswxgeauywfikztndhvjqr
+mbhposlxgeauywfikztnocmjqp
+mbvposfageauywfikztndcvjqr
+mbhpnslxgeauywfikztndgejqr
+mblposfxgeauypfikztndcvjqr
+mbhposlxyeauywfikzpndcvmqr
+ibhposlbgeauywfikotndcvjqr
+mbmposlxgeauywfiktwndcvjqr
+mbhposlxgeduywfikztndfvoqr
+mbhpoklxdeauywfikztndcvuqr
+mbhposlxgeauywfikltnlcvuqr
+mbhposlbgsauywfikztndsvjqr
+mbhposlxgeauywfirzfndcbjqr
+mphposlxgeauywfikztndcvjgg
+mohposlcgeauywfikzsndcvjqr
+mbhpovlxgeauyqfikotndcvjqr
+qbhpgslxgeauywqikztndcvjqr
+mbhposlxweauywfikztndtvjqm
+pbhposlxgeauywfikztnncvjqm
+mbbposlxpeauuwfikztndcvjqr
+mbhposlxgmauyrfikztndcvjir
+pbhposlqgefuywfikztndcvjqr
+mbhkoslxgeauywfikztndciwqr
+mbtpoflxgeauywfikztndrvjqr
+mbhcoslxveguywfikztndcvjqr
+mbhpovlxgeauywfhkdtndcvjqr
+mbhposlxgeauywftrztndcujqr
+mbhposlxgeaoywfdkzpndcvjqr
+mbnposlxgeyuywfikztldcvjqr
+mbaposlxweauywfikftndcvjqr
+mbhposljgeauywfikztcdcvvqr
+nbhpkslxgeauywfikzwndcvjqr
+mbhtoslxgeauywfikzkndcvjdr
+mbhposxxgeaxywfikztndsvjqr
+mbdpoflxgeauywfisztndcvjqr
+mbhposvxgeauywfikztnscvnqr
+mbcposlxghauywfikztndcgjqr
+mbhpovlxgeauywpckztndcvjqr
+mbhpfslxgeauywbikntndcvjqr
+mbhpowyxgeauywfikztndcvjcr
+mbhposlxoeatywfikztndcvoqr
+mchpfslxgeauywfikztidcvjqr
+mbhposvxgearywfikztndcvjcr
+mbhposlxgeauywfpcztnduvjqr
+mbhposlxgmauyyfiqztndcvjqr
+mbhposlxteauuwfikwtndcvjqr
+mbhpotlspeauywfikztndcvjqr
+mbhpoelxgeauywfikztndckjkr
+mbhpnslxgeauywfikztndcvkqs
+mbhpksfxgwauywfikztndcvjqr
+mxhwoglxgeauywfdkztndcvjqr
+mbhphsbjgeauywfikztndcvjqr
+mbhposlxgeauwifixztndcvjqr
+mbhposqxguauywfikztndcwjqr
+mbhposlngeauywfikztedcvjor
+nbhposlxgeauywiikztndcyjqr
+mbhposlxgeauawfykztndcvbqr
+mbhplslxgeauywcikztndcvjrr
+fshposlxgeagywfikztndcvjqr
+mbhposlxgeauymcikztndcxjqr
+mbhponlxgeauyloikztndcvjqr
+mbhposrxzeanywfikztndcvjqr
+mbhtoslxgeajyifikztndcvjqr
+mbhposixkeauywfikhtndcvjqr
+mahhoslxgeaufwfikztndcvjqr
+mbhpdslxteauywfikzvndcvjqr
+mfhposlxgeauywfiqttndcvjqr
+mbhplslxheauywfikztnscvjqr
+mbhpoylxgeauywbizztndcvjqr
+mbhposlhgeawywfjkztndcvjqr
+mbhkoslxgkauywfilztndcvjqr
+mbhposnxgeauywfikztkdcvlqr
+mvhpxslxgevuywfikztndcvjqr
+mbhpohlxgeauyrficztndcvjqr
+mbhsosuxgewuywfikztndcvjqr
+mbhpoxlxgeauywuikztnhcvjqr
+mbhposlxqeauyqfikztndcvrqr
+mbhpchlxgeauywfikztnhcvjqr
+mbhposlxgeauywoikztndcfqqr
+pbhposlxgeagmwfikztndcvjqr
+mxhwoglxgeauywfikztndcvjqr
+mbhpospxgaauywfikstndcvjqr
+mbhwoxlxgeauywfgkztndcvjqr
+mbhposlxgeauywfikvtndhvsqr
+mbbposlxgesuywfikztnicvjqr
+mhhjoslxgeauywfikztndccjqr
+mbhkoslxgeagywffkztndcvjqr
+mbhposlxgesqywfukztndcvjqr
+mbdposlxgeauywfilztndcvjqp
+mbhposlxgeakqwfikztedcvjqr
+mbhposuxgeayywficztndcvjqr
+mbhposlxgeauywfxkztndcloqr
+mchposlxgeauywoiiztndcvjqr
+tbhporlxgeauywfikztndcvyqr
+mbhposlxoevuywfikzindcvjqr
+qbhposlxfevuywfikztndcvjqr
+mbhposlxfeauvwfikztndcvgqr
+mbjposlxgsauywfikztnwcvjqr
+vbhposlxgeauvwfikztndcvjqk
+pbhposlxguauyrfikztndcvjqr
+mbhposlcgeauywfiketndcviqr
+mbsposlxgvauywfikztndcviqr
+mbhposlxgeauynfxkztndcvjbr
+mbhposlxtentywfikztndcvjqr
+mbhposlxgeavywfikztndhvjnr
+mbhpsvlxgeauywfikztndcvzqr
+mzhpotlxgeauywfiyztndcvjqr
+mbhposkqgeauywfiwztndcvjqr
+mbhposlxoeakywfikztndcvjqt
+mbhposlxghauywfikbdndcvjqr
+mbhpossxgeauywfikqxndcvjqr
+mbhposlxgearywhikztydcvjqr
+mbhposlxgeaiywfikztndfvjur
+mbhpxslxgoazywfikztndcvjqr
+mbhposlxneauywfibqtndcvjqr
+mnheoslxteauywfikztndcvjqr
+mbhposlxgeauywfmkztrdcvuqr
+mbhzowlxgeauywfizztndcvjqr
+mbhloslxgeauyofikztnucvjqr
+mbhposlxneagywfbkztndcvjqr
+mbhposongeauywfikztnzcvjqr
+mwyposlxgeauywfikztnqcvjqr
+mbhpnqaxgeauywfikztndcvjqr
+mboposlxzeauywfioztndcvjqr
+mbhposledeauywfikztndqvjqr
+mphpaslxgeauywfbkztndcvjqr
+mbhposrxgeauywlikbtndcvjqr
+ybhnoslxgeauywfihztndcvjqr
+mbhposlxgeauywfikntxccvjqr
+mbhposlxgeauqwfikutndcfjqr
+mbhposlxglabywfikztidcvjqr
+mbhpollxgeauywfikxtnscvjqr
+mboposlggeaufwfikztndcvjqr
+mbhposlxgeauywpikedndcvjqr
+mbhpoklxgeauywpikztndcvjlr
+mbhposhxgeauywfifztndcvpqr
+mbhposlxgwagywfikztndcvjwr
+mbhpokldgeauywfikztngcvjqr
+nbhposlxgeauywfiketndcvjxr
+mbhhoslxgexuywfikrtndcvjqr
+mbhposlxgefujwfikztkdcvjqr
+mbhposlxggagywfikztndchjqr
+mbhposlxgeauyvfilztnicvjqr
+mbhposlkgeauywfikzxndcvoqr
+mbhposlxgeauvqfikztndcvuqr
+zbhposlxgfauylfikztndcvjqr
+mbhyoshxgeauywfikztndcvjqa
+sbhposlxgeauyxfikztndavjqr
+mlhposlxgeauywfikzmndcqjqr
+mbhpaslxgekuywfikztnncvjqr
+ibhhoslxteauywfikztndcvjqr
+mbhposlxgeauyodibztndcvjqr
+mbhposlxgkaoywfikztndcvpqr
+mbhonslxgearywfikztndcvjqr
+mbdpoolxgealywfikztndcvjqr
+mbepfslxgvauywfikztndcvjqr
+mbhposlygeauywfikztfdcvaqr
+mthpoalxgeauywnikztndcvjqr
+mbhpesbogeauywfikztndcvjqr
+mbhposlxgjauywfikztnmcvjqj
+mbhnoslxgeauydfikgtndcvjqr
+mbhpxslxgeauywfikztndcvjcx
+muhposlxgwauywfipztndcvjqr
+mbhpcslxgeauywfiqztndcvjbr
+mbhpomlxgeauywfioftndcvjqr
+mbhposlfgepuywfikzmndcvjqr
+mbhsosliteauywfikztndcvjqr
+mbwposlxgeauywfikztnycveqr
+mbhpfslxgeauywfqkztndcvjhr
+mxhbvslxgeauywfikztndcvjqr
+fbhposlxgeauywfikzcnmcvjqr
+mbhfosfxgeauywfikztnduvjqr
+tbhporlxgeauywfikztndcvjqm
+mhhposlxgeauywfikctnecvjqr
+mbhposlxgeqtywfikztnmcvjqr
+qbhpjslxgeauywfikztndevjqr
+tbhpxslxgeaunwfikztndcvjqr
+wbhposlxgeadywfikztndcujqr
+mbhposlvgeauywfpkotndcvjqr
+mbhposlxgeagywfingtndcvjqr
+mbnposlxgeauywfikztnvcjjqr
+mohpoilxgeadywfikztndcvjqr
+mbhposlsgeauywfikztnxcvgqr
+mbhposlogeauywfikqtndcvjor
+mbhroslxgeauypfikztndcvjqg
+mblposuxgetuywfikztndcvjqr
+mbhposlogeiuywfikztodcvjqr
+mbhposlxgeauylfikztedcvrqr
+mbhfoslxgeautwxikztndcvjqr
+mbhouslxgeauywfikztnycvjqr
+mbhposlxgeauywfvkqtndlvjqr
+mbfposltgeauytfikztndcvjqr
+mbhposlxgcapywfikztnddvjqr
+hbhposlxgeasywfikztnxcvjqr
+mbhposntgeauywfikztcdcvjqr
+mbhponlxgfauywfirztndcvjqr
+mbhposlxgeatywlikztndcvrqr
+mohroslzgeauywfikztndcvjqr
+mbhpojaxgeauywfifztndcvjqr
+rbhposlxgwauywfikztndovjqr
+mbhpoclxgeaeywfikztndcvjqo
+mbhposllgeauywfikzondcvmqr
+mbhpxslxgeauywfikzymdcvjqr
+mbhposlxgeasywxikztndkvjqr
+mbhposlxgeauywfivztndcmjqx
+qbhposlxgpauywfikgtndcvjqr
+mbhposlxgeauyqdikztqdcvjqr
+cbhposlxgeauywfikttjdcvjqr
+mbhgoslxgeanywfihztndcvjqr
+mbhposlxgeajywfhkztndcvjvr
+mbhpozlxgeauewfmkztndcvjqr
+mbhposlxgeagywfbiztndcvjqr
+mbhmoslxgeauywfikztndrnjqr
+ybhposmxgeauywfikztndcviqr
+mrwposlxgeauywfikztndpvjqr
+mbhposlxneauywfikztndcbjqh
+mbhpowlxheauywfikztndcojqr
+mbeposlxgeauywfiwztnycvjqr
+mbhposixgeapywfikztndcvvqr
+mbhposlxgeauywfikztnbcvjap
+mzhposixgenuywfikztndcvjqr
+mbhposgxgeauywyikztndvvjqr
+mbhposajgeauywfikztzdcvjqr
+mbhyoslxgeauywfikzsndcvxqr
+mbhposlxgdauywfikmtndcljqr`;
+
+let aa = a.split('\n');
+
+let twos = 0;
+let threes = 0;
+
+for(let i = 0;i<aa.length;i++) {
+    let lets = {};
+    let s = aa[i];
+    for(let j = 0; j < s.length; j++) {
+        if(lets[s[j]]) {
+            lets[s[j]] ++;
+        } else {
+            lets[s[j]] = 1;
+        }
+    }
+    let ks = Object.keys(lets);
+    let two = false;
+    let three = false;
+    for(let j = 0; j < ks.length; j++) {
+        if(lets[ks[j]] == 2) {
+            two = true;
+        }
+        if(lets[ks[j]] == 3) {
+            three = true;
+        }
+    }
+    if(two) { twos++;};
+    if(three) { threes++};
+}
+//console.log(twos * threes);
+
+for(let i = 0; i < aa.length; i++) {
+    for(let j = i; j < aa.length; j++) {
+        let numDifferent = 0;
+        for(let si = 0; si < aa[i].length; si++) {
+            if(aa[i][si] != aa[j][si]) {
+                numDifferent++;
+            }
+        }
+        if(numDifferent == 1) {
+            console.log(aa[i] + "\n" + aa[j]);
+        }
+    }
+}
